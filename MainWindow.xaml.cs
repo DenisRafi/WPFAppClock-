d25ef614295efad41e5538888ba550f7d74a5b5a
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-11-16 18:47:31 +0000

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Samples.Misc
+{
+	public partial class Sample : Window
+	{
+		public Sample()
+		{
+			
+			DispatcherTimer timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromMilliseconds(1);
+			timer.Tick += timer_Tick;
+			timer.Start();
+		}
+		void timer_Tick(object sender, EventArgs e)
+		{
+			lblTime.Content = DateTime.Now.ToString("HH:mm:ss.fff");
+		}
+	}
+}
